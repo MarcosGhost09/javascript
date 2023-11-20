b119b1a75e56985f61c0441c2ba3a0bd9b7e960d
Sandeep Dotiyal <147370520+MarcosGhost09@users.noreply.github.com>
2023-11-20 15:10:48 +0000

    datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,21 @@
+ "use strict"; //treat all js code as newer version
+
+//  alert(3+3) we are nodejs not browser
+
+  let name = "hitesh"
+  let age = 19
+  let isloggedIn= false
+      
+
+//   number => 2 to power 53
+//  bigint
+//  string => ""
+// boolean => true/false
+// null or stand alone value
+// undefined value not define
+// symbol unqiueness kya liya
+// object
+// null type object hota hai
+// undefined type undefined hota hai 
+// 
+console.log(typeof "hitesh");
