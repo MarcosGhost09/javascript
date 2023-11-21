bc7a347d252074d1d58680aca7f86f77f0c8c60d
Sandeep Dotiyal <147370520+MarcosGhost09@users.noreply.github.com>
2023-11-21 04:58:31 +0000

    Rename variable

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,19 @@
+const accountName =144553
+let accountEmail = "sandeep@gmail.com"
+var accountPassword ="1234"
+// accountCity = "jaipur" This  is not good method for reserve a variable 
+
+let accountState;
+
+// accountName=2
+ 
+accountEmail="sanju@gmail.com"
+accountPassword="23232"
+accountCity="dehradun"
+
+console.log(accountState);
+  
+// prefer not to use var because of issue in block scope and functional scope
+
+// console.log(accountName);
+console.table([accountEmail,accountPassword,accountCity,accountState])
